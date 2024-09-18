33a31ad61d4c78bed4e38b2cbdf0fad122a9236b
Manny <mhfernan35277@gmail.com>
2024-09-17 23:58:29 -0700

    add file

--- server/app.py
+++ server/app.py
@@ -83,16 +83,27 @@ class CheckSession(Resource):
             return user.to_dict(), 200
         
         return {}, 401
+    
+    
 
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        
+        if not session['user_id']:
+            return {'error' : 'Unauthorized'}, 401
+        
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {'error' : 'Unauthorized'}, 401
+            
+        article = Article.query.filter(Article.id == id).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
